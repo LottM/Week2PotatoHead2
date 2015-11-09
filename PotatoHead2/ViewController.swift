f8871258b9fd6cdd34dbbe530884e19f03be97bc
LottM <lottevandermolen@gmail.com>
2015-11-09 15:35:29 +0100

    Buttons fixed

--- PotatoHead2/ViewController.swift
+++ PotatoHead2/ViewController.swift
@@ -8,64 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController { 
+class ViewController: UIViewController {
+    
     
     // change visibility of arms by using the switch
-    @IBOutlet weak var imageArms: UIImageView? ; internal var hiddenArms: Bool
+    @IBOutlet weak var imageArms: UIImageView!
+    
     @IBAction func switchArms(sender: UISwitch) {
         imageArms!.hidden = !self.imageArms!.hidden
     }
     
     // change visibility of ears by using the switch
-    @IBOutlet weak var imageEars: UIImageView? ; internal var hiddenEars: Bool
+    @IBOutlet weak var imageEars: UIImageView!
     @IBAction func switchEars(sender: AnyObject) {
         imageEars!.hidden = !self.imageEars!.hidden
         }
     
     // change visibility of eyebrows by using the switch
-    @IBOutlet weak var imageEyebrows: UIImageView? ; internal var hiddenEyebrows: Bool
+    @IBOutlet weak var imageEyebrows: UIImageView!
     @IBAction func switchEyebrows(sender: AnyObject) {
         imageEyebrows!.hidden = !self.imageEyebrows!.hidden
     }
     
     // change visibility of eyes by using the switch
-    @IBOutlet weak var imageEyes: UIImageView? ; internal var hiddenEyes: Bool
+    @IBOutlet weak var imageEyes: UIImageView!
     @IBAction func switchEyes(sender: AnyObject) {
         imageEyes!.hidden = !self.imageEyes!.hidden
     }
     
     // change visibility of glasses by using the switch
-    @IBOutlet weak var imageGlasses: UIImageView? ; internal var hiddenGlasses: Bool
+    @IBOutlet weak var imageGlasses: UIImageView!
     @IBAction func switchGlasses(sender: AnyObject) {
         imageGlasses!.hidden = !self.imageGlasses!.hidden
     }
     
     // change visibility of hat by using the switch
-    @IBOutlet weak var imageHat: UIImageView? ; internal var hiddenHat: Bool
+    @IBOutlet weak var imageHat: UIImageView!
     @IBAction func switchHat(sender: AnyObject) {
         imageHat!.hidden = !self.imageHat!.hidden
     }
     
     // change visibility of mouth by using the switch
-    @IBOutlet weak var imageMouth: UIImageView? ; internal var hiddenMouth: Bool
+    @IBOutlet weak var imageMouth: UIImageView!
     @IBAction func switchMouth(sender: AnyObject) {
         imageMouth!.hidden = !self.imageMouth!.hidden
     }
     
     // change visibility of mustache by using the switch
-    @IBOutlet weak var imageMustache: UIImageView? ; internal var hiddenMustache: Bool
+    @IBOutlet weak var imageMustache: UIImageView!
     @IBAction func switchMustache(sender: AnyObject) {
         imageMustache!.hidden = !self.imageMustache!.hidden
     }
     
     // change visibility of nose by using the switch
-    @IBOutlet weak var imageNose: UIImageView? ; internal var hiddenNose: Bool
+    @IBOutlet weak var imageNose: UIImageView!
     @IBAction func switchNose(sender: AnyObject) {
         imageNose!.hidden = !self.imageNose!.hidden
     }
     
     // change visibility of shoes by using the switch
-    @IBOutlet weak var imageShoes: UIImageView? ; internal var hiddenShoes: Bool
+    @IBOutlet weak var imageShoes: UIImageView!
     @IBAction func switchShoes(sender: AnyObject) {
         imageShoes!.hidden = !self.imageShoes!.hidden
     }
